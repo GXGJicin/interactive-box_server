2e24114c3e4575b6d99a035ce3e84ad6e9bce641
Martin Vancl <tux.martin@gmail.com>
2015-04-28 21:18:07 +0200

    funcni verze nastav.php z alixu
    ta stara nefungovala

--- php/okno/nastav.php
+++ php/okno/nastav.php
@@ -5,8 +5,14 @@ echo "<h1>Uspesne nastaveno!<h1>";
 echo "<h2><a href='/okno/'>Znovu!!!</a></h2>";
 
 
-$file = fopen("/dev/ttyS0","w");
+$file = fopen("/dev/ttyUSB0","w");
 //echo fwrite($file, "Hello World!");
+
+if ($_GET["vypinac"] == "1") {
+	echo fwrite($file, "<PISMENO,1,1,1,1,1,1,1>\n");
+} else {
+	echo fwrite($file, "<PISMENO,0,0,0,0,0,0,0>\n");
+}
 
 echo fwrite($file, "<RGB,");
 echo fwrite($file, $_GET["geekR"]. ",");
@@ -17,7 +23,7 @@ echo fwrite($file, $_GET["labR"]. ",");
 echo fwrite($file, $_GET["labG"]. ",");
 echo fwrite($file, $_GET["labB"]. ">\n");
 
-echo fwrite($file, "<PISMENO,1,1,1,1,1,1,1>\n");
+//echo fwrite($file, "<PISMENO,1,1,1,1,1,1,1>\n");
 
 fclose($file);
 
@@ -33,7 +39,12 @@ echo $_GET["labR"]. ",";
 echo $_GET["labG"]. ",";
 echo $_GET["labB"]. ">\n";
 
-echo "<PISMENO,1,1,1,1,1,1,1>\n";
+if ($_GET["vypinac"] == "1") {
+	echo "<PISMENO,1,1,1,1,1,1,1>\n";
+} 
+if ($_GET["vypinac"] == "0") {
+	echo "<PISMENO,0,0,0,0,0,0,0>\n";
+}
 
 echo "</xmp>";
 
